28de8646237e080a2708332d7d9feea3da259c73
mahir <mahir@10.0.0.214>
2023-03-26 11:28:44 -0400

    Hello team!

---
+++ BookstoreApp/src/backendModel/goldMember.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backendModel;
+
+/**
+ *
+ * @author mahir
+ */
+public class goldMember implements State{
+
+    @Override
+    public double calculateDiscount(double bookPrice, int currentPoints) {
+        //check if the customer can buy the book
+           if(bookPrice * 100 <= currentPoints){
+               //the book is bought with points only
+               currentPoints = (int) (currentPoints - (bookPrice *100.00));
+               return bookPrice;
+           } else if(bookPrice*100 > currentPoints) {
+               //Only a part of the price is payed off with points
+               bookPrice = bookPrice - currentPoints/100;
+               //REMEMBER TO GIVE POINTS FOR BOUGHT BOOK
+               return bookPrice;
+           } else{
+               return 0;
+           }
+           
+    }
+        //REMEMBER TO IMPLEMENTS THE CHANGE TO SILVER IF LESS THAN 1k
+    
+}
