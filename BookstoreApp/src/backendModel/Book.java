fe559642fa108483cd5e8e4096b65f7bcbdee335
adrianyu91 <adrianyu91@gmail.com>
2023-03-27 00:15:26 -0400

    Completed Book Class

--- BookstoreApp/src/backendModel/Book.java
+++ BookstoreApp/src/backendModel/Book.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package backendModel;
 
 /**
@@ -10,5 +5,37 @@ package backendModel;
  * @author adria
  */
 public class Book {
+    private String name;
+    private double price;
+    static public int id;
+    private boolean isChecked = false;
+    
+    
+    //Book constructor
+    public Book(String name, double price){
+        this.name = name;
+        this.price = price;
+        this.isChecked = false;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
+    public void setCheck(){
+        isChecked = !isChecked;
+    }
+    public boolean getCheck(){
+        return isChecked;
+    }
+    
     
 }
